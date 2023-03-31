8d8c11ba0bf6b055e73f63d82be6efb5d4ca3920
pedroalcantara-net <pedrohla42@gmail.com>
2023-03-31 08:40:23 -0300

    Commit Inicial Login Stateful

---
+++ StatefulLogin/StatefulLogin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatefulLogin.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Username") == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                ViewBag.Username = HttpContext.Session.GetString("Username");
+                return View();
+            }
+        }
+    }
+}
---
+++ StatefulLogin/StatefulLogin/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StatefulLogin.ViewModels;
+
+namespace StatefulLogin.Controllers
+{
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Username") == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel login)
+        {
+            if (login.Username == "admin" && login.Password == "admin")
+            {
+                HttpContext.Session.SetString("Username", login.Username);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Error = "Usuário ou senha inválidos!";
+                return View();
+            }
+        }
+
+        [HttpGet("Logout")]
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
